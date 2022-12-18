71fff84bc6d5bfd61bf157584cc9f45feb553c7f
TatianaYakushonok <yakush2@yandex.ru>
2022-12-18 16:26:13 +0700

    feat: add cart for project (Sun, 18 Dec 2022 16:26:13 +0700)

--- shop-burger/dist/js/navigationListController.js
+++ shop-burger/dist/js/navigationListController.js
@@ -4,7 +4,7 @@ import {
     navigationListItems
 } from './elements.js';
 
-export const navigationListController = () => {
+export const navigationListController = (cb) => {
     navigationList.addEventListener('click', (e) => {
         const categoryItem = e.target.closest('.navigation__button');
 
@@ -14,6 +14,7 @@ export const navigationListController = () => {
             if(el === categoryItem) {
                 el.classList.add('navigation__button_active');
                 catalogTitle.textContent = el.textContent;
+                cb(el.dataset.category);
             }else {
                 el.classList.remove('navigation__button_active');
             };
